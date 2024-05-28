e21b961446320c48bdc06e3dec7b40001a32331e
rendifitrianda <rendifitrianda95@gmail.com>
2024-05-28 12:22:25 +0700

    managament template navbar

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -43,15 +43,50 @@
 <body>
 
     <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top d-flex align-items-center">
+    <header id="header" class="header fixed-top d-flex align-items-center" style="background: #053070">
 
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
                 <img src="/assets/assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">P3M</span>
+                <span class="d-none d-lg-block" style="color: white">P3M</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
+        <span style="width: 150px">
+            <div class="dropdown-center">
+                <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"
+                    style="color: white">
+                    Monitoring
+                </div>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Usulan Reguler</a></li>
+                    <li><a class="dropdown-item" href="#">Perbaikan Usulan</a></li>
+                    <li><a class="dropdown-item" href="#">Catatan harian</a></li>
+                    <li><a class="dropdown-item" href="#">Laporan Kemajuan</a></li>
+                    <li><a class="dropdown-item" href="#">Laporan Akhir</a></li>
+                    <li><a class="dropdown-item" href="#">Usulan Prototipe</a></li>
+                    <li><a class="dropdown-item" href="#">Usulan Kosabangsa Kolaborasi</a></li>
+                    <li><a class="dropdown-item" href="#">Monev internal PT</a></li>
+                    <li><a class="dropdown-item" href="#">Minat katalis</a></li>
+                </ul>
+            </div>
+        </span>
+
+
+        <span>
+            <div class="dropdown-center">
+                <div class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"
+                    style="color: white">
+                    Data Pendukung
+                </div>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Profile Lembaga</a></li>
+                    <li><a class="dropdown-item" href="#">Cari akun</a></li>
+                    <li><a class="dropdown-item" href="#">Singkronasi Prodi</a></li>
+                    <li><a class="dropdown-item" href="#">Singkronasi Dosen</a></li>
+                </ul>
+            </div>
+        </span>
 
         <nav class="header-nav ms-auto">
             <ul class="d-flex align-items-center">
@@ -62,12 +97,13 @@
                     </a>
                 </li><!-- End Search Icon-->
 
+
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                         data-bs-toggle="dropdown">
                         <img src="/assets/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                        <span class="d-none d-md-block dropdown-toggle ps-2" style="color: white">K. Anderson</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -118,12 +154,9 @@
 
                     </ul><!-- End Profile Dropdown Items -->
                 </li><!-- End Profile Nav -->
-
             </ul>
         </nav><!-- End Icons Navigation -->
-
     </header><!-- End Header -->
-
     <!-- ======= Sidebar ======= -->
     <aside id="sidebar" class="sidebar">
 
