ddeda5d5ba5978cba17d6833d6375ba95566d7be
rendifitrianda <rendifitrianda95@gmail.com>
2024-05-29 10:55:32 +0700

    manage template new

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="/assets/assets/img/favicon.png" rel="icon">
-    <link href="/assets/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ url('assets') }}/assets/img/favicon.png" rel="icon">
+    <link href="{{ url('assets') }}/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
@@ -20,16 +20,16 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="/assets/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="/assets/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="/assets/assets/css/style.css" rel="stylesheet">
+    <link href="{{ url('assets') }}/assets/css/style.css" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -42,64 +42,53 @@
 
 <body>
 
-    <!-- ======= Header ======= -->
-    <header id="header" class="header fixed-top d-flex align-items-center" style="background: #053070">
+    <x-template.header />
 
-        <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
-                <img src="/assets/assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block" style="color: white">P3M</span>
-            </a>
-            {{-- <i class="bi bi-list toggle-sidebar-btn" style="color: white"></i> --}}
-        </div><!-- End Logo -->
-
-        <span style="width: 120px">
-            <div>
-                <div type="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white">
-                    Dashboard
-                </div>
-            </div>
-        </span>
-
-
-
-        <x-template.monitoring />
-        <x-template.data_pendukung />
-        <div class="content">
-            <div class="wrapper">
-                <div class="content-wrapper">
-                    <div class="content">
-                        <div class="container-fluid pt-3">
-                            {{ $slot }}
+    <x-template.sidebar />
+    <main id="main" class="main">
+        <section class="section">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Area Chart</h5>
                         </div>
                     </div>
                 </div>
             </div>
+        </section>
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
         </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
-        <x-template.sidebar />
-        <x-template.navbar />
-
-    </header><!-- End Header -->
-    <!-- ======= Sidebar ======= -->
-
-
+    </footer><!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="/assets/assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="/assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="/assets/assets/vendor/echarts/echarts.min.js"></script>
-    <script src="/assets/assets/vendor/quill/quill.js"></script>
-    <script src="/assets/assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="/assets/assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="/assets/assets/vendor/php-email-form/validate.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/quill/quill.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="{{ url('assets') }}/assets/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
-    <script src="/assets/assets/js/main.js"></script>
+    <script src="{{ url('assets') }}/assets/js/main.js"></script>
 
 </body>
 
--- resources/views/components/template/sidebar.blade.php
+++ resources/views/components/template/sidebar.blade.php
@@ -1,37 +1,29 @@
-{{-- <aside id="sidebar" class="sidebar">
-
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
-
         <li class="nav-item">
             <a class="nav-link collapsed" href="index.html">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li><!-- End Dashboard Nav -->
-
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="icons-bootstrap.html">
-                        <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+                    <a href="tables-general.html">
+                        <i class="bi bi-circle"></i><span>General Tables</span>
                     </a>
                 </li>
                 <li>
-                    <a href="icons-remix.html">
-                        <i class="bi bi-circle"></i><span>Remix Icons</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="icons-boxicons.html">
-                        <i class="bi bi-circle"></i><span>Boxicons</span>
+                    <a href="tables-data.html">
+                        <i class="bi bi-circle"></i><span>Data Tables</span>
                     </a>
                 </li>
             </ul>
-        </li><!-- End Icons Nav -->
-
+        </li><!-- End Tables Nav -->
     </ul>
-
-</aside><!-- End Sidebar--> --}}
+</aside><!-- End Sidebar-->
